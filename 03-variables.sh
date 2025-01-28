4d408d6c84600a8f826cf245061ff500a971e6b6
prabhu vaddi <prabhuvaddidaws81s@gmail.com>
2025-01-28 23:55:20 +0530

    adding variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=prudhvi
+person2=prabhu
+
+echo $person1: "hi $person2 how r u"
+echo $person2: "hi $person1 i am good what about u"
+echo $person1: "Thanks for asking i am also good"
