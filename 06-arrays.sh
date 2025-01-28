f4e42109d2a98974826b056766e3d49f7d9c59aa
prabhu vaddi <prabhuvaddidaws81s@gmail.com>
2025-01-29 01:07:26 +0530

    adding args

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,7 +2,7 @@
 
 Fruits=("apple" "mango" "orange")
 
-echo "first fruit is : $Fruits[0]"
-echo "second fruit is : $Fruits[1]"
-echo "third fruit is : $Fruits[2]"
-echo "All fruits are : $Fruits[@]"
+echo "first fruit is : ${Fruits[0]}"
+echo "second fruit is : ${Fruits[1]}"
+echo "third fruit is : ${Fruits[2]}"
+echo "All fruits are : ${Fruits[@]}"
