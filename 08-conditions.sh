3362aaadc29b60b3b3790b2da330d186d371f3a6
prabhu vaddi <prabhuvaddidaws81s@gmail.com>
2025-02-08 05:14:37 +0530

    adding args

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 number=$1
- if [ $number -gt 20 ]
- then
+if [ $number -gt 20 ]
+then
 echo "Given number: $number is greater than 20"
 else
 echo "Given number: $number is less than 20"
